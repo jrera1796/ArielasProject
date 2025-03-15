3ff91b6b83f999733eef2ab8e410ad0a14a254eb
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2025-03-14 23:55:33 -0500

    Test auto-deploy 3

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -73,7 +73,7 @@ const LandingPage = () => {
         </p>
         <div className="staff-login-container">
           <Link to="/staff" className="staff-login">
-            Stafff Login
+            Stafffff Login
           </Link>
         </div>
       </footer>
